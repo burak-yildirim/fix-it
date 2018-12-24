2762d6fbe6725cd1dfee92f5539e4f861ba499b7
burak-yildirim <yildirim.brk.94@gmail.com>
2018-12-24 20:43:27 +0300

    code in core.forms cleaned

--- core/forms.py
+++ core/forms.py
@@ -9,7 +9,7 @@ class BaseModelForm(forms.ModelForm):
     """
 
     def __init__(self, *args, **kwargs):
-        super(BaseModelForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = "form-control"
 
